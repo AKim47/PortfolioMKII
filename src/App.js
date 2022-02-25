c716be2a25efc243bffe6985f262349306b8fd2e
AKim47 <abel.kim.22@gmail.com>
2022-02-24 23:15:28 -0600

    basic stylings

--- src/App.js
+++ src/App.js
@@ -33,6 +33,7 @@ function App() {
 
       </Header>
       <main>
+        
 
       </main>
       <Footer></Footer>
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,16 +2,18 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer className="w-100 mt-auto bg-secondary p-4">
-      <div className="container">
-          <a href="https://github.com/AKim47">GitHub link</a>
-      </div>
-      <div className="container">
-          <a href="https://www.linkedin.com/in/abel-kim-9988691a8/">LinkedIn link</a>
-      </div>
-      <div className="container">
-          <a href="https://www.facebook.com/profile.php?id=100017623033945">Facebook link</a>
-      </div>
+    <footer >
+      <ul className="flex-row px-1">
+          <li classname="mx-1">
+            <a href="https://github.com/AKim47">GitHub</a>
+          </li>
+          <li classname="mx-1">
+          <a href="https://www.linkedin.com/in/abel-kim-9988691a8/">LinkedIn</a>
+          </li>
+          <li classname="mx-1">
+          <a href="https://www.facebook.com/profile.php?id=100017623033945">Facebook</a>
+          </li>
+      </ul>
     </footer>
   );
 };
